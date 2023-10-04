01293a6a1467a38cbf579ab895749f455b1bb9fd
한성민 <123073840+anxi01@users.noreply.github.com>
2023-10-04 20:15:05 +0900

    [Bronze II] Title: 트로피 진열, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        System.out.println(count(list));
+        Collections.reverse(list);
+        System.out.println(count(list));
+
+    }
+    public static int count(List<Integer> list){
+        int now = list.get(0);
+        int result = 1;
+
+        for(int i = 1; i < list.size(); i++){
+            if(now < list.get(i)){
+                result += 1;
+                now = list.get(i);
+            }
+        }
+        return result;
+    }
+}
