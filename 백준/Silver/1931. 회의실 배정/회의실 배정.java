9e2c0f7d7bb24a39c078ff722dcd1ec65da4756a
한성민 <123073840+anxi01@users.noreply.github.com>
2023-10-12 23:02:19 +0900

    [Silver I] Title: 회의실 배정, Time: 1296 ms, Memory: 175288 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -24,6 +24,8 @@ public class Main {
             int end = sc.nextInt();
             data.add(new Time(start, end));
         }
+        // 끝나는 시간을 기준으로 오름차순 정렬
+        // 만약 끝나는 시간이 같다면 시작 시간을 기준으로 오름차순 정렬
         data.sort(new Comparator<Time>() {
             @Override
             public int compare(Time o1, Time o2) {
@@ -35,7 +37,7 @@ public class Main {
             }
         });
 
-        int count = 0;
+/*        int count = 0;
         int tmp = 0;
 
         for(int i = 0; i < n; i++){
@@ -43,6 +45,18 @@ public class Main {
                 count++;
                 tmp = data.get(i).end;
             }
+        }
+        System.out.println(count);*/
+
+        int end_time = data.get(0).end;
+        int count = 1;
+        for(int i = 1; i < n; i++){
+            int start_time = data.get(i).start;
+            if(start_time < end_time){
+                continue;
+            }
+            end_time = data.get(i).end;
+            count++;
         }
         System.out.println(count);
     }
