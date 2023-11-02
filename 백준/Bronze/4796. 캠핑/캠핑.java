b06c08a4febf8cb91bac9b42539408aa031b1c3c
한성민 <123073840+anxi01@users.noreply.github.com>
2023-11-02 23:45:36 +0900

    [Bronze I] Title: 캠핑, Time: 336 ms, Memory: 19748 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.java
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int count = 0;
 
-        while (true) {
+        while(true){
+
             int L = sc.nextInt();
             int P = sc.nextInt();
             int V = sc.nextInt();
-            if (L==0 && P ==0 && V ==0) break;
-            count++;
-            int X = 0;
-            X = (V/P)*L;
 
-            if (V%P <=L) {
-                X += V%P;
-            } else {
-                X +=L;
+            if(L == 0 && P == 0 && V == 0) break;
+
+            // ex) 1 20 39
+            // 최대 1일 + 남는 19일 + 최대 1일 + 남는 19일(over) => 1 + 1
+
+            // ex) 5 8 20
+            // 최대 5일 + 남는 3일 + 최대 5일 + 남는 3일 + 최대 4일 => 5 + 5 + 4
+            int sum = 0;
+            sum = (V / P) * L;
+            count++;
+
+            if(V % P <= L){
+                sum += V % P;
+            }else{
+                sum += L;
             }
-            System.out.println("Case " + count + ": " + X);
+            System.out.println("Case " + count + ": " + sum);
         }
     }
 }
