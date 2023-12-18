72eed2dd648f2091a1e08726f50f298438f14b37
한성민 <123073840+anxi01@users.noreply.github.com>
2023-12-19 03:29:15 +0900

    [Silver III] Title: 소수 구하기, Time: 1276 ms, Memory: 28848 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -20,6 +20,7 @@ public class Main {
     public static boolean isPrime(int number) {
         if(number == 1) return false;
 
+        // 제곱근을 이용한 기본 판별법
         for (int i = 2; i <= Math.sqrt(number); i++) {
 
             if(number % i == 0) return false;
