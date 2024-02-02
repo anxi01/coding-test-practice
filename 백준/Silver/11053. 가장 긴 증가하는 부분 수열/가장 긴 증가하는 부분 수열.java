be21f0226340858380064b29f478011542b8f4cc
한성민 <123073840+anxi01@users.noreply.github.com>
2024-02-02 15:40:08 +0900

    Update 가장 긴 증가하는 부분 수열.java

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,39 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-      int n = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
 
-      int[] seq = new int[n];
+    int[] seq = new int[n];
 
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < n; i++) {
-        seq[i] = Integer.parseInt(st.nextToken());
-      }
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      seq[i] = Integer.parseInt(st.nextToken());
+    }
 
-      int[] dp = new int[n];
+    int[] dp = new int[n];
+    int result = 0;
 
-      /**
-       * seq : 10 20 10 30 20 50
-       * dp  : 1  2  1  3  2  4
-       */
-      for (int i = 0; i < n; i++) {
-        dp[i] = 1;
+    for (int i = 0; i < n; i++) {
+      dp[i] = 1;
 
-        for (int j = 0; j < i; j++) {
-          if (seq[j] < seq[i] && dp[i] < dp[j] + 1) {
-            dp[i] = dp[j] + 1;
-          }
+      for (int j = 0; j < i; j++) {
+        if (seq[j] < seq[i]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
         }
       }
-      Arrays.sort(dp);
-      System.out.println(dp[n - 1]);
+      result = Math.max(result, dp[i]);
     }
+    System.out.println(result);
+  }
 }
