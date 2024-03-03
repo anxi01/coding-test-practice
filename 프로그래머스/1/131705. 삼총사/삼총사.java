c20fa1d58574a336f7868466a83cb54c44c70b6e
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-03 23:15:39 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int solution(int[] number) {
+    int answer = 0;
+
+    for (int i = 0; i < number.length - 2; i++) {
+      for (int j = i + 1; j < number.length - 1; j++) {
+        for (int p = j + 1; p < number.length; p++) {
+          if (number[i] + number[j] + number[p] == 0) {
+            answer++;
+          }
+        }
+      }
+    }
+    return answer;
+  }
+}
