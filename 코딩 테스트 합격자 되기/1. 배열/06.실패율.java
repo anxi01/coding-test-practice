92db28cc2214bdeccd206f132daad224df056753
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-14 23:12:43 +0900

    06.실패율.java 정답 코드

--- 코딩 테스트 합격자 되기/1. 배열/06.실패율.java
+++ 코딩 테스트 합격자 되기/1. 배열/06.실패율.java
@@ -1,3 +1,39 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+  public static int[] solution(int N, int[] stages) {
+
+    // 스테이지별 도전자 수 (1부터 N+1)
+    // 스테이지가 N일 경우, N번째 스테이지를 클리어한 사용자는 N+1번째 인덱스에 저장하기 때문이다.
+    int[] challenger = new int[N + 2];
+    for (int i = 0; i < stages.length; i++) {
+      challenger[stages[i]] += 1;
+    }
+
+    // 스테이지별 실패한 사용자 수 계산
+    HashMap<Integer, Double> fails = new HashMap<>();
+    double total = stages.length;
+
+    // 각 스테이지를 순회하며 실패율 계산
+    for (int i = 1; i <= N; i++) {
+      if (challenger[i] == 0) {
+        fails.put(i, 0.); // 도전한 사람이 없는 경우 실패율은 0
+      } else {
+        fails.put(i, challenger[i] / total); // 실패율 계산
+        total -= challenger[i]; // 다음 스테이지 실패율을 구하기 위해 현재 스테이지의 인원을 뺌
+      }
+    }
+
+    // 실패율이 높은 스테이지부터 내림차순으로 정렬
+    return fails.entrySet().stream()
+        .sorted(((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())))
+        .mapToInt(HashMap.Entry::getKey).toArray();
+        // Entry : key + value <-> keySet : key만 
+  }
+}
+
+/*
 // 기존 코드
 class Solution {
   public static int[] solution(int N, int[] stages) {
@@ -36,4 +72,4 @@ class Solution {
     return result;
   }
 }
-// 결과 : [3, 3, 2, 1, 5], 정답 : [3,4,2,1,5]
+*/
