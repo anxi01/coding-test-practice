73f109ca457e9aaa30d78f6a6763d1d13fbb7218
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-24 02:49:34 +0900

    answer: 11.짝지어 제거하기.java

--- 코딩 테스트 합격자 되기/2. 스택/11.짝지어 제거하기.java
+++ 코딩 테스트 합격자 되기/2. 스택/11.짝지어 제거하기.java
@@ -1,3 +1,27 @@
+import java.util.ArrayDeque;
+
+// 정답코드
+class Solution {
+  public static int solution(String s) {
+
+    ArrayDeque<Character> stack = new ArrayDeque<>();
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char c = s.charAt(i);
+
+      // 스택이 비어 있지 않고, 현재 문자와 스택의 맨 위 문자가 같으면
+      if (!stack.isEmpty() && stack.peek() == c) { 
+        stack.pop(); // 스택의 맨 위 문자 제거
+      } else {
+        stack.push(c); // 스택에 현재 문자 추가
+      }
+    }
+    return stack.isEmpty() ? 1 : 0; // 스택이 비어있으면 1 그렇지 않으면 0
+  }
+}
+
+/*
 // 실패코드
 class Solution {
   public static int solution(String s) {
@@ -27,3 +51,4 @@ class Solution {
     }
   }
 }
+*/
