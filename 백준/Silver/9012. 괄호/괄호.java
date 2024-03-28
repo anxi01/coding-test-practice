d16d667af2218212598d205eed30e5bd9d584df9
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-28 17:37:28 +0900

    [Silver IV] Title: 괄호, Time: 144 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < n; i++) {
-            String[] data = br.readLine().split("");
-            Stack<String> stack = new Stack<>();
+    int n = Integer.parseInt(br.readLine());
 
-            for(String s : data) {
-                if (Objects.equals(s, "(")) {
-                    stack.push(s);
-                } else if (Objects.equals(s, ")")) {
-                    if (stack.isEmpty()) {
-                        stack.push(s);
-                        break;
-                    } else {
-                        stack.pop();
-                    }
-                }
-            }
-            if (stack.isEmpty()) {
-                System.out.println("YES");
-            }else {
-                System.out.println("NO");
-            }
-        }
+    for (int i = 0; i < n; i++) {
+      String s = br.readLine();
+      System.out.println(isValidVPS(s));
     }
+  }
+
+  static String isValidVPS(String s) {
+    ArrayDeque<Character> stack = new ArrayDeque<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        stack.push('(');
+      } else {
+        if (stack.isEmpty() || stack.pop() == s.charAt(i)) {
+          return "NO";
+        }
+      }
+    }
+
+    if (stack.isEmpty()) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
 }
