1774cabe4284c46b3b4e562b9fb713ce899a4d23
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-28 13:06:25 +0900

    answer: 12.주식가격.java
    O(N^2), 스택 사용 X

--- 코딩 테스트 합격자 되기/2. 스택/12.주식가격.java
+++ 코딩 테스트 합격자 되기/2. 스택/12.주식가격.java
@@ -1,20 +1,21 @@
-import java.util.ArrayDeque;
 import java.util.Arrays;
 
 class Solution {
   public static int[] solution(int[] prices) {
-    ArrayDeque<Integer> stack = new ArrayDeque<>();
+
     int[] answer = new int[prices.length];
 
+    // O(N^2)
+    // 입력값 : 100,000
+    // 총 10,000,000,000 (100억)
+    // 만약 1억에 1초라고 가정하면 100초가 걸림
     for (int i = 0; i < prices.length; i++) {
-      stack.push(prices[i]);
-
       for (int j = i + 1; j < prices.length; j++) {
-        if (!stack.isEmpty() && stack.peek() <= prices[j]) {
-          answer[i]++;
+        answer[i]++;
+        if (prices[i] > prices[j]) {
+          break;
         }
       }
-      stack.pop();
     }
     return answer;
   }
