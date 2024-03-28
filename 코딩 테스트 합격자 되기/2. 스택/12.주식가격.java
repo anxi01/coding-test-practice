fa3690840a3d7505e1751a21fd6750023dbc5a40
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-28 12:01:59 +0900

    wrong: 12.주식가격.java

--- 코딩 테스트 합격자 되기/2. 스택/12.주식가격.java
+++ 코딩 테스트 합격자 되기/2. 스택/12.주식가격.java
@@ -1,1 +1,21 @@
-// 투포인터 문제
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+class Solution {
+  public static int[] solution(int[] prices) {
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    int[] answer = new int[prices.length];
+
+    for (int i = 0; i < prices.length; i++) {
+      stack.push(prices[i]);
+
+      for (int j = i + 1; j < prices.length; j++) {
+        if (!stack.isEmpty() && stack.peek() <= prices[j]) {
+          answer[i]++;
+        }
+      }
+      stack.pop();
+    }
+    return answer;
+  }
+}
