3ab82ec2ab2a42768893695c9067d716130c82cd
한성민 <123073840+anxi01@users.noreply.github.com>
2024-03-29 21:14:51 +0900

    [level 2] Title: 주식가격, Time: 15.86 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,21 +1,31 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
 
 class Solution {
   public static int[] solution(int[] prices) {
+    int n = prices.length;
+    int[] answer = new int[n]; // 가격이 떨어지지 않은 기간을 저장할 배열
 
-    int[] answer = new int[prices.length];
+    // 스택을 사용해 이전 가격과 현재 가격 비교
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+    stack.push(0); // 0번째 index 먼저 집어넣음 -> 비교 기준 값
 
-    // O(N^2)
-    // 입력값 : 100,000
-    // 총 10,000,000,000 (100억)
-    // 만약 1억에 1초라고 가정하면 100초가 걸림
-    for (int i = 0; i < prices.length; i++) {
-      for (int j = i + 1; j < prices.length; j++) {
-        answer[i]++;
-        if (prices[i] > prices[j]) {
-          break;
-        }
+    // O(N)
+    for (int i = 1; i < n; i++) {
+      while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+        // 이전 가격보다 현재 가격이 낮을 때 (즉 가격이 떨어졌을 떄)
+        // 이전 가격의 기간 계산
+        int j = stack.pop();
+        answer[j] = i - j;
       }
+      stack.push(i);
+    }
+
+    // 스택에 끝까지 남은 index는
+    // 가장 끝의 index(n-1)에서 현재 index(j) 값을 빼서 구한다.
+    while (!stack.isEmpty()) {
+      int j = stack.pop();
+      answer[j] = n - 1 - j;
     }
     return answer;
   }
