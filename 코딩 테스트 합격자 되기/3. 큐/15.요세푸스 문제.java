6a65d9f66a82c2db3d8b6fb838d383a38a585c7e
한성민 <123073840+anxi01@users.noreply.github.com>
2024-04-07 16:11:22 +0900

    answer: 15.요세푸스 문제.java

--- 코딩 테스트 합격자 되기/3. 큐/15.요세푸스 문제.java
+++ 코딩 테스트 합격자 되기/3. 큐/15.요세푸스 문제.java
@@ -4,17 +4,18 @@ class Solution {
   static int solution(int n, int k) {
     ArrayDeque<Integer> queue = new ArrayDeque<>();
 
+    // 1부터 n까지의 번호를 queue에 추가
     for (int i = 1; i <= n; i++) {
       queue.addLast(i);
     }
-
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = 0; j < k - 1; j++) {
-        if (!queue.isEmpty()) {
-          queue.addLast(queue.pollFirst());
-        }
+    
+    // queue에 하나의 요소가 남을 때까지 반복
+    while (queue.size() > 1) {
+      // k번째 요소를 찾기 위해 앞에서부터 제거하고 뒤에 추가
+      for (int i = 0; i < k - 1; i++) {
+        queue.addLast(queue.pollFirst());
       }
-      queue.pollFirst();
+      queue.pollFirst(); // k번째 요소 제거
     }
 
     return queue.pollFirst();
