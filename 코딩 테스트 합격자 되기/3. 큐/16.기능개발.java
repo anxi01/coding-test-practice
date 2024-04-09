a5a81ea86609d69a473c2e72ed14068d8fc5bfcd
한성민 <123073840+anxi01@users.noreply.github.com>
2024-04-09 17:12:21 +0900

    answer: 16.기능개발.java

--- 코딩 테스트 합격자 되기/3. 큐/16.기능개발.java
+++ 코딩 테스트 합격자 되기/3. 큐/16.기능개발.java
@@ -1,36 +1,37 @@
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Queue;
 
 class Solution {
   public static int[] solution(int[] progresses, int[] speeds) {
 
-    ArrayDeque<Integer> queue = new ArrayDeque<>();
+    Queue<Integer> queue = new ArrayDeque<>();
 
-    for (int i = 0; i < progresses.length; i++) {
-      int count = 0;
-      while (progresses[i] < 100) {
-        progresses[i] += speeds[i];
+    int n = progresses.length;
+
+    // 각 작업의 배포 가능일 계산
+    int[] dayLeft = new int[n];
+    for (int i = 0; i < n; i++) {
+      // Math.ceil (올림) , Math.floor (내림)
+      dayLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+    }
+
+    int count = 0; // 배포될 작업의 수 카운트
+    int maxDay = dayLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포될 작업의 가능일
+
+    for (int i = 0; i < n; i++) {
+      // 배포 가능일이 가장 늦은 배포일보다 빠르면
+      if (dayLeft[i] <= maxDay) {
         count++;
-      }
-      queue.addLast(count);
-    }
-
-    ArrayList<Integer> temp = new ArrayList<>();
-    ArrayList<Integer> result = new ArrayList<>();
-    while (!queue.isEmpty()) {
-      int current = queue.pollFirst();
-      temp.add(current);
-
-      if (!queue.isEmpty() && current < queue.peekFirst()) {
-        result.add(temp.size());
-        temp = new ArrayList<>();
-      }
-
-      if (queue.isEmpty()) {
-        result.add(temp.size());
+      } else {
+        // 배포 예정일이 기준 배포일보다 느리면
+        queue.add(count);
+        count = 1;
+        maxDay = dayLeft[i];
       }
     }
-    return result.stream().mapToInt(Integer::intValue).toArray();
+
+    queue.add(count); // 마지막으로 카운트된 작업들을 함께 배포
+    return queue.stream().mapToInt(Integer::intValue).toArray();
   }
 }
