6fad91a85b4ab733823c0b845d94904f165071fc
한성민 <123073840+anxi01@users.noreply.github.com>
2024-04-12 00:25:56 +0900

    answer: 17.카드뭉치.java

--- 코딩 테스트 합격자 되기/3. 큐/17.카드뭉치.java
+++ 코딩 테스트 합격자 되기/3. 큐/17.카드뭉치.java
@@ -1,28 +1,36 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Queue;
 
 class Solution {
     public static String solution(String[] cards1, String[] cards2, String[] goal) {
 
-        Queue<String> cards1Queue = new ArrayDeque<>(Arrays.asList(cards1));
-        Queue<String> cards2Queue = new ArrayDeque<>(Arrays.asList(cards2));
+        /*
+        Todo. Queue가 비었을 경우는 탐색하지 않는 조건을 달아줘야 함.
+         */
 
-        for (int i = 0; i < goal.length; i++) {
-            if (Objects.equals(cards1Queue.peek(), goal[i])) {
-                cards1Queue.poll();
-            } else if (Objects.equals(cards2Queue.peek(), goal[i])) {
-                cards2Queue.poll();
-            } else {
-                return "No";
+        // cards와 goal을 queue로 변환
+        Queue<String> cardsQueue1 = new ArrayDeque<>(Arrays.asList(cards1));
+        Queue<String> cardsQueue2 = new ArrayDeque<>(Arrays.asList(cards2));
+        Queue<String> goalQueue = new ArrayDeque<>(Arrays.asList(goal));
+
+        // goalQueue에 문자열이 남아 있으면 계속 반복
+        while (!goalQueue.isEmpty()) {
+            // cardQueue1의 front와 일치하는 경우
+            if (!cardsQueue1.isEmpty() && cardsQueue1.peek().equals(goalQueue.peek())) {
+                cardsQueue1.poll();
+                goalQueue.poll();
+            }
+            // cardQueue2의 front와 일치하는 경우
+            else if (!cardsQueue2.isEmpty() && cardsQueue2.peek().equals(goalQueue.peek())) {
+                cardsQueue2.poll();
+                goalQueue.poll();
+            }
+            else {
+                break; // 일치하는 원소가 없을 경우 종료
             }
         }
 
-        if (cards1Queue.isEmpty() && cards2Queue.isEmpty()) {
-            return "Yes";
-        } else {
-            return "No";
-        }
+        return goalQueue.isEmpty() ? "Yes" : "No";
     }
 }
