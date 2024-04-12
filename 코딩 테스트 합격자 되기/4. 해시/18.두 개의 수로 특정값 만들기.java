578cf70bc1f488f6c8454ac8f20b4efdbe873f0a
한성민 <123073840+anxi01@users.noreply.github.com>
2024-04-12 17:21:05 +0900

    answer: 18.두 개의 수로 특정값 만들기.java
    O(N) 방법

--- 코딩 테스트 합격자 되기/4. 해시/18.두 개의 수로 특정값 만들기.java
+++ 코딩 테스트 합격자 되기/4. 해시/18.두 개의 수로 특정값 만들기.java
@@ -1,14 +1,23 @@
+import java.util.HashSet;
+
 class Solution {
     public static boolean solution(int[] arr, int target) {
 
-        // O(N^2) 풀이 -> 입력 값이 10,000이기 때문에 시간 초과
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) {
-                    return true;
-                }
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        // target - arr의 원소이 해시셋에 존재하면 true
+        // 그렇지 않으면 현재 arr의 원소를 넣기
+        
+        for (int i : arr) {
+            // target에서 현재 원소를 뺀 값이 해시셋에 있는지 확인
+            if (hashSet.contains(target - i)) {
+                return true;
             }
+
+            // 해시셋에 현재 값 저장
+            hashSet.add(i);
         }
+
         return false;
     }
 }
