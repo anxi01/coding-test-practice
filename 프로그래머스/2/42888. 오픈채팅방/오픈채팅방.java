bd6dc52b2f47eea35e6e013d1cb61146deb16ded
한성민 <123073840+anxi01@users.noreply.github.com>
2024-04-19 20:38:09 +0900

    [level 2] Title: 오픈채팅방, Time: 93.12 ms, Memory: 170 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+  public static String[] solution(String[] record) {
+
+    final String ENTER = "Enter";
+    final String LEAVE = "Leave";
+    final String CHANGE = "Change";
+
+    final String ENTER_KOR = "님이 들어왔습니다.";
+    final String LEAVE_KOR = "님이 나갔습니다.";
+
+    HashMap<String, String> map = new HashMap<>();
+
+    for (String r : record) {
+      String[] tmp = r.split(" ");
+      if (tmp[0].equals(ENTER) || tmp[0].equals(CHANGE)) {
+        map.put(tmp[1], tmp[2]);
+      }
+    }
+
+    List<String> list = new ArrayList<>();
+    for (String r : record) {
+      String[] tmp = r.split(" ");
+
+      switch (tmp[0]) {
+        case ENTER:
+          list.add(map.get(tmp[1]) + ENTER_KOR);
+          break;
+        case LEAVE:
+          list.add(map.get(tmp[1]) + LEAVE_KOR);
+      }
+    }
+
+    return list.toArray(new String[0]);
+  }
+}
