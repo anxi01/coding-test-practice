365b62ada4ee47c435d8e9025b7cbf782fada894
한성민 <123073840+anxi01@users.noreply.github.com>
2024-05-02 01:34:30 +0900

    update: 22.베스트앨범.java
    교재 정답

--- 코딩 테스트 합격자 되기/4. 해시/22.베스트앨범.java
+++ 코딩 테스트 합격자 되기/4. 해시/22.베스트앨범.java
@@ -1,56 +1,41 @@
-/*
-풀이과정
-  1. 스트리밍 횟수를 각 장르별로 저장한다.
-  2. 장르를 key로 가진 map을 생성하는데 그 map의 value는 고유번호를 가진 키와 그 재생 횟수를 값으로 가지는 map이다.
-  3. 우선 장르 별로 재생 횟수가 높은 것부터 최대 2개씩 뽑을 수 있으므로, 재생 횟수 map(1번)의 keySet을 내림차순으로 저장한다.
-  4. 장르를 순회하면서 최대 2개씩 answer 리스트에 넣는다.
-*/
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 class Solution {
   public static int[] solution(String[] genres, int[] plays) {
 
-    HashMap<String, Integer> totalMap = new HashMap<>();
+    HashMap<String, Integer> playMap = new HashMap<>();
+    HashMap<String, ArrayList<int[]>> genreMap = new HashMap<>();
 
-    for (int i = 0; i < genres.length; i++) {
-      totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
-    }
-
-    HashMap<String, HashMap<Integer, Integer>> genreCountMap = new HashMap<>();
-
-    for (int i = 0; i < plays.length; i++) {
-      if (!genreCountMap.containsKey(genres[i])) {
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        countMap.put(i, plays[i]);
-        genreCountMap.put(genres[i], countMap);
-      } else {
-        genreCountMap.get(genres[i]).put(i, plays[i]);
+    // 장르별 총 재생 횟수와 각 곡의 재생 횟수 저장
+    for(int i = 0; i < genres.length; i++) {
+      String genre = genres[i];
+      int play = plays[i];
+      if (!genreMap.containsKey(genre)) {
+        genreMap.put(genre, new ArrayList<>());
+        playMap.put(genre, 0);
       }
+      genreMap.get(genre).add(new int[]{i, play});
+      playMap.put(genre, playMap.get(genre) + play);
     }
-
-    List<String> keySet = new ArrayList<>(totalMap.keySet());
-    // 값을 기준으로 내림차순 정렬
-    keySet.sort(((o1, o2) -> totalMap.get(o2) - totalMap.get(o1)));
 
     List<Integer> answer = new ArrayList<>();
-    for (String key : keySet) {
-      HashMap<Integer, Integer> countMap = genreCountMap.get(key);
-      List<Integer> genreKeys = new ArrayList<>(countMap.keySet());
 
-      // 값을 기준으로 내림차순 정렬
-      genreKeys.sort(((o1, o2) -> countMap.get(o2) - countMap.get(o1)));
+    // 총 재생 횟수가 많은 장르순으로 내림차순 정렬
+    Stream<Map.Entry<String, Integer>> sortedGenre = playMap.entrySet().stream().sorted(((o1, o2) ->
+        Integer.compare(o2.getValue(), o1.getValue())));
 
-      // 각 장르 별로 최대 2곡만 가능하다.
-      answer.add(genreKeys.get(0));
-      if (genreKeys.size() > 1) {
-        answer.add(genreKeys.get(1));
-      }
-    }
-
-    return answer.stream().mapToInt(s -> s).toArray();
+    // 각 장르 내에서 노래를 재생 횟수 순으로 정렬해 최대 2곡까지 선택
+    sortedGenre.forEach(entry -> {
+      Stream<int[]> sortedSongs = genreMap.get(entry.getKey()).stream()
+          .sorted(((o1, o2) -> Integer.compare(o2[1], o1[1])))
+          .limit(2);
+      sortedSongs.forEach(song -> answer.add(song[0]));
+    });
+    return answer.stream().mapToInt(Integer::intValue).toArray();
   }
 }
