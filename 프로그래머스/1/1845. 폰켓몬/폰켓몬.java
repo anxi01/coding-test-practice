dc626dd187fbb34b666571989343c4e664b862ed
성민 <123073840+anxi01@users.noreply.github.com>
2024-12-19 02:24:34 +0900

    [level 1] Title: 폰켓몬, Time: 6.76 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return Math.min(max, set.size());
+    }
+}
