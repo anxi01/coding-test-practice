f29e1f8ed07ca68ffdbf84320cfd9a6127090775
성민 <123073840+anxi01@users.noreply.github.com>
2025-02-16 00:07:57 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 604 ms, Memory: 50748 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -17,21 +17,21 @@ public class Main {
     StringTokenizer st;
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
-      arr[i][1] = Integer.parseInt(st.nextToken());
       arr[i][0] = Integer.parseInt(st.nextToken());
+      arr[i][1] = Integer.parseInt(st.nextToken());
     }
 
     Arrays.sort(arr, (e1, e2) -> {
-      if (e1[0] == e2[0]) {
-        return e1[1] - e2[1];
+      if (e1[1] == e2[1]) {
+        return Integer.compare(e1[0], e2[0]);
       } else {
-        return e1[0] - e2[0];
+        return Integer.compare(e1[1], e2[1]);
       }
     });
 
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < N; i++) {
-      sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
+      sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
     }
     System.out.println(sb);
   }
