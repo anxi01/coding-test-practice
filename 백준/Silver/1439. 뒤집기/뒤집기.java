d8e64db887c25705744ae27e8481650c1a049553
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-06-30 17:33:12 +0900

    [Silver V] Title: 뒤집기, Time: 96 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -7,25 +7,48 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        char[] data = input.toCharArray();
-        
-        // 전부 0으로 바꾸는 경우
-        int count0 = 0;
-        // 전부 1로 바꾸는 경우
-        int count1 = 0;
-        
-        if(data[0] == '0')
-            count1 += 1;
-        else count0 += 1;
+        String S = br.readLine();
 
-        for(int i = 0; i < data.length - 1; i++){
-            if(data[i] != data[i+1]){
-                if(data[i + 1] == '1'){
-                    count0 += 1;
-                }else count1 += 1;
+        StringBuilder all0 = new StringBuilder();
+        StringBuilder all1 = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            all0.append("0");
+            all1.append("1");
+        }
+
+        if (S.equals(all0.toString()) || S.equals(all1.toString())) {
+            System.out.print(0);
+            return;
+        }
+
+        int count0 = 0;
+        int count1 = 0;
+
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '1') {
+                if (i >= 1 && S.charAt(i - 1) == '0') {
+                    count1++;
+                }
+                if (i == S.length() - 1) {
+                    count0++;
+                }
+            }
+
+            if (S.charAt(i) == '0') {
+                if (i >= 1 && S.charAt(i - 1) == '1') {
+                    count0++;
+                }
+                if (i == S.length() - 1) {
+                    count1++;
+                }
             }
         }
-        System.out.println(Math.min(count0, count1));
+
+        if (count0 == 0 || count1 == 0) {
+            System.out.print(Math.max(count0, count1));
+            return;
+        }
+
+        System.out.print(Math.min(count0, count1));
     }
 }
