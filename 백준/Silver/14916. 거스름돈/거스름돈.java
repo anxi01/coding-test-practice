de7dc76d7939516b12a2d1f81cdb71faccd9d4f7
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-07-01 18:04:40 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,30 +1,31 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    private static final int COIN2 = 2;
+    private static final int COIN5 = 5;
 
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int count = 0;
-        int count5 = n / 5;
-        while (true) {
-            int tmp = n;
-            tmp -= count5 * 5;
+        // int 최댓값으로 초기화
+        int minCount = Integer.MAX_VALUE;
 
-            if (tmp % 2 == 0) {
-                count += tmp / 2;
-                count += count5;
-                System.out.println(count);
-                break;
-            }else {
-                count5--;
-                if (count5 < 0) {
-                    System.out.println(-1);
-                    break;
-                }
+        // 사용할 수 있는 5원 동전의 최대 개수
+        int maxCoin5Count = n / COIN5;
+
+        // 5원 동전을 0개부터 최대 개수까지 사용하는 모든 경우 순회
+        for (int i = 0; i <= maxCoin5Count; i++) {
+            int copyN = n - COIN5 * i; // 5원 동전을 사용한 후 남은 금액
+
+            // 남은 금액이 2원으로 정확히 나누어떨어질 경우만 처리
+            if (copyN % COIN2 == 0) {
+                int count = i + (copyN / COIN2); // 5원 + 2원 동전 개수의 합
+                minCount = Math.min(minCount, count);
             }
         }
+
+        System.out.println(minCount == Integer.MAX_VALUE ? -1 : minCount);
     }
 }
