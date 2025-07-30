318ac521f0ccd1d163f4f0443fb6f16e62f25f88
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-07-30 17:33:02 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,44 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
+	
+	private static int[][] computer;
 
-    private static boolean[] visited;
+	private static boolean[] visited; // 방문 배열 생성
 
-    private static int[][] computer;
+	public int solution(int n, int[][] computers) {
+		computer = computers; // shallow copy
+		visited = new boolean[n];
 
-    private static void dfs(int now) {
-        visited[now] = true;
-        for (int i = 0; i < computer[now].length; i++) {
-            if (computer[now][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
+		int answer = 0;
 
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        visited = new boolean[n];
-        computer = computers;
+		// 모든 컴퓨터 순회
+		for (int i = 0; i < n; i++) {
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                answer++;
-            }
-        }
-        return answer;
-    }
+			// 방문하지 않은 경우에만 dfs 수행
+			if (!visited[i]) {
+				dfs(i);
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+
+	private static void dfs(int now) {
+		// 방문한 정점은 다시 돌지 않기
+		if (visited[now]) {
+			return;
+		}
+
+		visited[now] = true;
+
+		for (int i = 0; i < computer.length; i++) {
+			// 컴퓨터와 네트워크 연결이 되어있을 경우, dfs 수행
+			if (computer[now][i] == 1) {
+				dfs(i);
+			}
+		}
+	}
 }
