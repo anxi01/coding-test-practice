51db3801fedda8a2bc5408749c4b61feb15f837f
SeongMin Han <123073840+anxi01@users.noreply.github.com>
2025-07-31 14:56:57 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    private static List<Integer>[] adjList;
+    private static List<List<Integer>> computerNet = new ArrayList<>();
+
     private static boolean[] visited;
-    private static int cnt;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 컴퓨터 수(노드)
-        int m = Integer.parseInt(br.readLine()); // 간선 수
 
-        adjList = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            adjList[i] = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        int P = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            computerNet.add(new ArrayList<>());
         }
 
-        StringTokenizer st;
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adjList[a].add(b);
-            adjList[b].add(a);
+        for (int i = 0; i < P; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int c1 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            computerNet.get(c1).add(c2); // 무방향그래프
+            computerNet.get(c2).add(c1); // 무방향그래프
         }
 
-        visited = new boolean[n + 1];
-
-        cnt = 0;
         dfs(1);
-        
-        // dfs 하면서 노드 탐색 시 cnt를 1씩 증가하였음
-        // 1번 컴퓨터는 cnt를 제외해야 하므로 1을 빼줌
-        System.out.println(cnt - 1);
+
+        int answer = 0;
+        for (int i = 2; i <= N; i++) {
+            if (visited[i]) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
     }
 
     private static void dfs(int now) {
-        visited[now] = true;
-        cnt++;
+        if (visited[now]) {
+            return;
+        }
 
-        for (int next : adjList[now]) {
-            if (!visited[next]) {
-                dfs(next);
-            }
+        visited[now] = true;
+
+        for (int next : computerNet.get(now)) {
+            dfs(next);
         }
     }
 }
